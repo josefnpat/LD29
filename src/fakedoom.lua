fc790a7db53368cfc51a28a957ed1a6ce300196d
josefnpat <josef.patoprsty@redbottledesign.com>
2014-04-26 23:47:28 -0400

    making default more normal

--- src/fakedoom.lua
+++ src/fakedoom.lua
@@ -15,7 +15,7 @@ function fakedoom.load_wall(name,color)
   return data
 end
 
-fakedoom.load_wall("default",{255,0,255})
+fakedoom.load_wall("default",{31,31,31})
 fakedoom.load_wall("dirt",{89,66,47})
 fakedoom.load_wall("lava",{255,0,0})
 fakedoom.load_wall("moss",{49,128,28})
